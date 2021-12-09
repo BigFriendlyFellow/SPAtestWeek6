746f9694887ef2f47a7ff94e21216b42cbc9c608
Joe Ranzetta <jranzetta@prodigy.net>
2021-12-08 18:31:22 -0600

    website is working

---
+++ components/views/Home.js
@@ -0,0 +1,10 @@
+import html from "html-literal";
+
+export default () => html`
+  <section id="jumbotron">
+    <h2>Savvy Coders November 2021 Cohort</h2>
+    <a href="" onClick="alert('Hello! You clicked button!')"
+      >"Call to Action" "Button"</a
+    >
+  </section>
+`;
